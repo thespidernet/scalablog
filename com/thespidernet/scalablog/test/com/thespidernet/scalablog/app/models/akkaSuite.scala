a34d8c660c3dab98e253d7613f7b1ef8a7aa07e7
Gavin Baumanis <gavinb@thespidernet.com>
2015-08-31 11:56:24 +1000

    Quick FIx.

--- com/thespidernet/scalablog/test/com/thespidernet/scalablog/app/models/akkaSuite.scala
+++ com/thespidernet/scalablog/test/com/thespidernet/scalablog/app/models/akkaSuite.scala
@@ -70,22 +70,19 @@ package silentactor02 {
 			with StopSystemAfterAll {
 
 		"A Silent Actor" must {
-			//<start id="ch02-silentactor-test02"/>
 			"change internal state when it receives a message, single" in {
-				import SilentActor._ //<co id="ch02-silentactor-test02-import-protocol"/>
+				import SilentActor._ //import-protocol
 
-				val silentActor = TestActorRef[SilentActor] //<co id="ch02-silentactor-test02-TestActorRef"/>
+				val silentActor = TestActorRef[SilentActor] //TestActorRef
 				silentActor ! SilentMessage("whisper")
-				silentActor.underlyingActor.state must (contain("whisper")) //<co id="ch02-silentactor-test02-assert-state"/>
+				silentActor.underlyingActor.state must (contain("whisper"))
 			}
-			//<end id="ch02-silentactor-test02"/>
 		}
 	}
 
   
-	//<start id="ch02-silentactor-test02-imp"/>
-	object SilentActor { //<co id="ch02-silentactor-test02-protocol"/>
-		case class SilentMessage(data: String) //<co id="ch02-silentactor-test02-message"/>
+	object SilentActor { 
+		case class SilentMessage(data: String) //test message"/>
 		case class GetState(receiver: ActorRef)
 	}
 
@@ -95,13 +92,12 @@ package silentactor02 {
 
 		def receive = {
 			case SilentMessage(data) =>
-				internalState = internalState :+ data //<co id="ch02-silentactor-test02-internal-state"/>
+				internalState = internalState :+ data //internal state
 		}
 
-		def state = internalState //<co id="ch02-silentactor-test02-internal-state-method"/>
+		def state = internalState //internal state method"/>
 	}
 }
-//<end id="ch02-silentactor-test02-imp"/>
 
 
 package silentactor03 {
@@ -112,26 +108,23 @@ package silentactor03 {
 			with StopSystemAfterAll {
 
 		"A Silent Actor" must {
-			//<start id="ch02-silentactor-test03"/>
 			"change internal state when it receives a message, multi" in {
-				import SilentActor._ //<co id="ch02-silentactor-test03-import-protocol"/>
+				import SilentActor._ //import protocol
 
-				val silentActor = system.actorOf(Props[SilentActor], "s3") //<co id="ch02-silentactor-test03-create-actor"/>
+				val silentActor = system.actorOf(Props[SilentActor], "s3") //create actor"/>
 				silentActor ! SilentMessage("whisper1")
 				silentActor ! SilentMessage("whisper2")
-				silentActor ! GetState(testActor) //<co id="ch02-silentactor-test03-get-state"/>
-				expectMsg(Vector("whisper1", "whisper2")) //<co id="ch02-silentactor-test03-expectMsg"/>
+				silentActor ! GetState(testActor) //get state
+				expectMsg(Vector("whisper1", "whisper2")) //expectMsg
 			}
-			//<end id="ch02-silentactor-test03"/>
 		}
 
 	}
 
   
-	//<start id="ch02-silentactor-test03-imp"/>
 	object SilentActor {
 		case class SilentMessage(data: String)
-		case class GetState(receiver: ActorRef) //<co id="ch02-silentactor-test03-getstate-msg"/>
+		case class GetState(receiver: ActorRef) //getstate msg"/>
 	}
 
 	class SilentActor extends Actor {
@@ -141,8 +134,7 @@ package silentactor03 {
 		def receive = {
 			case SilentMessage(data) =>
 				internalState = internalState :+ data
-			case GetState(receiver) => receiver ! internalState //<co id="ch02-silentactor-test03-process-getstate-msg"/>
+			case GetState(receiver) => receiver ! internalState //process getstate msg
 		}
 	}
-	//<end id="ch02-silentactor-test03-imp"/>
 }
