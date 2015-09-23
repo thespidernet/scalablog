d25b72c2936d9e1c0f79c99680c8aabc6e060770
gavinbaumanis <gavinbaumanis@gmail.com>
2015-09-24 02:08:26 +1000

    Ensure that we have all the appropriate imports for the new PLAY templates helpers.

--- app/controllers/com/thespidernet/scalablog/Users.scala
+++ app/controllers/com/thespidernet/scalablog/Users.scala
@@ -66,6 +66,12 @@ package controllers
 import play.api._
 import play.api.mvc._
 import java.util.Date._
+import play.api.i18n._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import play.api.libs.json.Json
+
 import models.com.thespidernet.scalablog.SystemUser
 import models.com.thespidernet.scalablog.NormalUser
 
